84a145774a2dc85acde70a07445c8f46901f7ad6
fufex3934 <fufawakgari174@gmail.com>
2025-07-29 16:50:25 +0300

    implement jwt strategy

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
+import { TokenPayload } from '../interface/payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('JWT_SECRET') as string,
+    });
+  }
+  validate(payload: TokenPayload) {
+    return payload;
+  }
+}
