9c9a20556e5addd791d38dd36cc263220f14ff08
fufex3934 <fufawakgari174@gmail.com>
2025-08-02 15:59:39 +0300

    finalize task crud

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { FilterTaskDto } from './dto/filter-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Post()
+  async create(@Body() createTaskDto: CreateTaskDto) {
+    return await this.tasksService.create(createTaskDto);
+  }
+
+  @Get()
+  async findAll(@Query() filterTaskDto: FilterTaskDto) {
+    return await this.tasksService.findAll(filterTaskDto);
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.tasksService.findOne(id);
+  }
+  @Patch(':id')
+  async update(@Body() updateTaskDto: UpdateTaskDto, @Param('id') id: string) {
+    return await this.tasksService.update(id, updateTaskDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this.tasksService.remove(id);
+  }
+}
